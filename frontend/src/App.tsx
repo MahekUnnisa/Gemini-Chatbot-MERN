0f8c5f3d8c5f72dd2e9fe61d9c66a1c3ccfcf73c
mahek unnisa <mahekunnisa786@gmail.com>
2024-02-18 21:23:12 +0530

    Frontend - Added components - Login, Signup, Header, Logo, Chat, NotFound, Home Todo: - Header - Integrate context API for state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Home from "./pages/Home"
+import Chat from "./pages/Chat"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import NotFound from "./pages/NotFound"
+import Header from "./components/Header"
+import { Route, Routes } from "react-router-dom"
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <main>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </main>
 }
 
 export default App
